13194923619d89beeab08ea0f0e20d794fcd5fe7
Jonathan Franklin <jonathan.t.franklin@gmail.com>
2017-01-31 11:05:26 -0700

    Created Unity scripts and new objects

---
+++ First Steps in Unity/Assets/move.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class move : MonoBehaviour 
+{
+
+	// Use this for initialization
+	void Start () 
+	{
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		
+		// If left arrow is pressed, move object -.5 units on x axis.
+		if (Input.GetKey (KeyCode.LeftArrow)) 
+		{
+			gameObject.transform.Translate (-.5f, 0, 0);
+		}
+
+		// If up arrow is pressed, move object .5 units on y axis.
+		if (Input.GetKey (KeyCode.UpArrow)) 
+		{
+			gameObject.transform.Translate (0, .5f, 0);
+		}
+
+		// If right arrow is pressed, move object .5 units on x axis.
+		if (Input.GetKey (KeyCode.RightArrow)) 
+		{
+			gameObject.transform.Translate (.5f, 0, 0);
+		}
+
+		// If down arrow is pressed, move object -.5 units on y axis.
+		if (Input.GetKey (KeyCode.DownArrow)) 
+		{
+			gameObject.transform.Translate (0, -.5f, 0);
+		}
+
+	}
+
+}
---
+++ First Steps in Unity/Assets/rotate.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class rotate : MonoBehaviour 
+{
+    public float speed;
+
+	// Use this for initialization
+	void Start () 
+    {
+        speed = Random.Range(.5f, 2.0f);
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+        // Provides simple rotation to an object.
+        transform.Rotate(new Vector3(15, 30, 45) * Time.deltaTime * speed);
+	}
+}
