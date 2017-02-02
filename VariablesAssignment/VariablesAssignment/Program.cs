5cbf776a837a814622175414031d061a4f2c7175
Jonathan Franklin <jonathan.t.franklin@gmail.com>
2017-02-02 10:17:14 -0700

    Created Variables C# Project

---
+++ VariablesAssignment/VariablesAssignment/Program.cs
@@ -0,0 +1,24 @@
+﻿// File Prologue
+// Name: Jonathan Franklin
+// Project: Variables
+// Date: February 2, 2017
+
+
+// I declare that the following source code was written by me, or provided
+// by the instructor for this project. I understand that copying 
+// source code from any other source constitutes cheating, and that I will
+// receive a zero grade on this project if I am found in violation of
+// this policy.
+
+using System;
+
+namespace VariablesAssignment
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			Console.WriteLine ("Hello World!");
+		}
+	}
+}
