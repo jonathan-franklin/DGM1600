afeb25cc31021199cd79b5fede5de3621c50bccd
Jonathan Franklin <jonathan.t.franklin@gmail.com>
2017-02-16 11:09:16 -0700

    Added Switch Assignment

---
+++ First Steps in Unity/Assets/SwitchAssignment.cs
@@ -0,0 +1,37 @@
+﻿// File Prologue
+// Name: Jonathan Franklin
+// Project: Switch Assignment
+// Date: February 16, 2017
+
+// I declare that the following source code was written by me, or provided
+// by the instructor for this project. I understand that copying 
+// source code from any other source constitutes cheating, and that I will
+// receive a zero grade on this project if I am found in violation of
+// this policy.
+
+/// <summary>
+/// The purpose of this script is to take 
+/// </summary>
+/// 
+
+
+using UnityEngine;
+using System.Collections;
+
+
+
+public class SwitchAssignment : MonoBehaviour 
+{
+
+	// Use this for initialization
+	void Start () 
+	{
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+	
+	}
+}
