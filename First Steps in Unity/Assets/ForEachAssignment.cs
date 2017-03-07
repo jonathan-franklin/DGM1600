cf5a6e18cd695bf455efc58873dcd6bbd3e4691a
Jonathan Franklin <jonathan.t.franklin@gmail.com>
2017-03-07 11:02:08 -0700

    ForEach example

--- First Steps in Unity/Assets/ForEachAssignment.cs
+++ First Steps in Unity/Assets/ForEachAssignment.cs
@@ -4,14 +4,51 @@ using System.Collections;
 public class ForEachAssignment : MonoBehaviour 
 {
 	public List<string> animals;
+	public List<string> dogs;
+	public List<string> cats;
 
 	// Use this for initialization
 	void Start () 
 	{
-	foreach (string animal in animals)
-	{
-		print (animal);
-	}
+		foreach (string animal in animals)
+		{
+			print (animal);
+
+			switch (animal) 
+			{
+				case "cat":
+					cats.Add (animal);
+					animals.Remove (animal);
+					break;
+
+				case "dog":
+					dogs.Add (animal);
+					animals.Remove (animal);
+					break;
+
+				default:
+					break;
+			}
+		}
+
+		foreach (string removeAnimal in animals)
+		{
+			print (removeAnimal);
+
+			switch (removeAnimal)
+			{
+				case "cat":
+					cats.Remove (animal);
+					break;
+
+				case "dog":
+					dogs.Remove (animal);
+					break;
+
+				default:
+					break;	
+			}
+		}
 	}
 	
 	// Update is called once per frame
