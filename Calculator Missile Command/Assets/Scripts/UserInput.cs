0afaf75928d80d08793ffc9f3da34ba3af33a0b4
Jonathan Franklin <jonathan.t.franklin@gmail.com>
2017-04-13 01:52:50 -0600

    Building main game UI

--- Calculator Missile Command/Assets/Scripts/UserInput.cs
+++ Calculator Missile Command/Assets/Scripts/UserInput.cs
@@ -1,16 +1,30 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
-public class UserInput : MonoBehaviour {
+public class UserInput : MonoBehaviour
+{
+    public InputField functionInputField;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void clickNumberButton(string numberClicked)
+    {
+        string currentString = functionInputField.text;
+        string newString = currentString + numberClicked; ;
+
+        functionInputField.text = newString;
+
+    }
 }
