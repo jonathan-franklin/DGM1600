da9e0a4ba264c96e324719c4f37692a7b6223125
Jonathan Franklin <jonathan.t.franklin@gmail.com>
2017-04-20 22:01:12 -0600

    Got calculator section mostly running

---
+++ Calculator Missile Command/Assets/Scripts/TargetBehavior.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TargetBehavior : MonoBehaviour
+{
+
+	// Use this for initialization
+	void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        TargetMovement();
+    }
+
+    public void TargetMovement()
+    {
+        transform.Translate(Vector3.down, Space.World);
+    }
+}
--- Calculator Missile Command/Assets/Scripts/UserInput.cs
+++ Calculator Missile Command/Assets/Scripts/UserInput.cs
@@ -6,6 +6,11 @@ using UnityEngine.UI;
 public class UserInput : MonoBehaviour
 {
     public InputField functionInputField;
+    public int inputOne = 0;
+    public int inputTwo = 0;
+    public int currentInput = 0;
+    public string currentOperator;
+    public int result;
 
     // Use this for initialization
     void Start()
@@ -19,12 +24,148 @@ public class UserInput : MonoBehaviour
 
     }
 
-    public void clickNumberButton(string numberClicked)
+    public void clickNumberButton (string numberClicked)
     {
         string currentString = functionInputField.text;
-        string newString = currentString + numberClicked; ;
+        string newString = currentString + numberClicked;
+
+        if (numberClicked == "1")
+        {
+            currentInput = currentInput * 10 + 1;
+        }
+        else if (numberClicked == "2")
+        {
+            currentInput = currentInput * 10 + 2;
+        }
+        else if (numberClicked == "3")
+        {
+            currentInput = currentInput * 10 + 3;
+        }
+        else if (numberClicked == "4")
+        {
+            currentInput = currentInput * 10 + 4;
+        }
+        else if (numberClicked == "5")
+        {
+            currentInput = currentInput * 10 + 5;
+        }
+        else if (numberClicked == "6")
+        {
+            currentInput = currentInput * 10 + 6;
+        }
+        else if (numberClicked == "7")
+        {
+            currentInput = currentInput * 10 + 7;
+        }
+        else if (numberClicked == "8")
+        {
+            currentInput = currentInput * 10 + 8;
+        }
+        else if (numberClicked == "9")
+        {
+            currentInput = currentInput * 10 + 9;
+        }
+        else if (numberClicked == "0")
+        {
+            currentInput = currentInput * 10;
+        }
+        else if (numberClicked == "+" && inputOne == 0)
+        {
+            inputOne = currentInput;
+            currentOperator = "+";
+            currentInput = 0;
+        }
+        else if (numberClicked == "-" && inputOne == 0)
+        {
+            inputOne = currentInput;
+            currentOperator = "-";
+            currentInput = 0;
+        }
+        else if (numberClicked == "*" && inputOne == 0)
+        {
+            inputOne = currentInput;
+            currentOperator = "*";
+            currentInput = 0;
+        }
+        else if (numberClicked == "/" && inputOne == 0)
+        {
+            inputOne = currentInput;
+            currentOperator = "/";
+            currentInput = 0;
+        }
+        else if (numberClicked == "%" && inputOne == 0)
+        {
+            inputOne = currentInput;
+            currentOperator = "%";
+            currentInput = 0;
+        }
+        else if (numberClicked == "Enter" && inputOne != 0 && inputTwo == 0 && currentOperator == "+")
+        {
+            inputTwo = currentInput;
+            result = inputOne + inputTwo;
+            print(result);
+
+            inputOne = 0;
+            inputTwo = 0;
+            currentInput = 0;
+            currentOperator = "";
+
+                        newString = "" + result;
+        }
+        else if (numberClicked == "Enter" && inputOne != 0 && inputTwo == 0 && currentOperator == "-")
+        {
+            inputTwo = currentInput;
+            result = inputOne - inputTwo;
+            print(result);
+
+            inputOne = 0;
+            inputTwo = 0;
+            currentInput = 0;
+            currentOperator = "";
+
+            newString = "" + result;
+        }
+        else if (numberClicked == "Enter" && inputOne != 0 && inputTwo == 0 && currentOperator == "*")
+        {
+            inputTwo = currentInput;
+            result = inputOne * inputTwo;
+            print(result);
+
+            inputOne = 0;
+            inputTwo = 0;
+            currentInput = 0;
+            currentOperator = "";
+
+            newString = "" + result;
+        }
+        else if (numberClicked == "Enter" && inputOne != 0 && inputTwo == 0 && currentOperator == "/")
+        {
+            inputTwo = currentInput;
+            result = inputOne / inputTwo;
+            print(result);
+
+            inputOne = 0;
+            inputTwo = 0;
+            currentInput = 0;
+            currentOperator = "";
+
+            newString = "" + result;
+        }
+        else if (numberClicked == "Enter" && inputOne != 0 && inputTwo == 0 && currentOperator == "%")
+        {
+            inputTwo = currentInput;
+            result = inputOne % inputTwo;
+            print(result);
+
+            inputOne = 0;
+            inputTwo = 0;
+            currentInput = 0;
+            currentOperator = "";
+
+            newString = "" + result;
+        }
 
         functionInputField.text = newString;
-
     }
+
 }
